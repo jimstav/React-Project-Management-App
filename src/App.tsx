b48fa87c5240628430625a9cc7898a9f1b7750f1
Dimitris Stavroulakis <jimstav@gmail.com>
2025-12-28 17:47:45 +0200

    Initial Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { useState } from 'react';
+import Projects from './Projects';
+import NoProjectSelected from './NoProjectSelected';
+import ProjectDetails from './ProjectDetails';
+
+export type Project = {
+  title: string;
+  description: string;
+  dueDate: Date;
+};
+
 function App() {
-  return <></>;
+  const [selectedProject, setSelectedProject] = useState<Project | null>(null);
+
+  return (
+    <>
+      <div className="grid grid-flow-col auto-cols-max h-screen">
+        <aside className="p-4 bg-black text-white rounded">
+          <Projects />
+        </aside>
+        <main className="p-4">
+          {selectedProject ? (
+            <ProjectDetails project={selectedProject} />
+          ) : (
+            <NoProjectSelected />
+          )}
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/ProjectDetails.tsx
@@ -0,0 +1,11 @@
+import type { Project } from './App';
+
+interface ProjectDetailsProps {
+  project: Project;
+}
+
+const ProjectDetails = ({ project }: ProjectDetailsProps) => {
+  return <div>ProjectDetails</div>;
+};
+
+export default ProjectDetails;
