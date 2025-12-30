844f923d0fbd7bd721160f49fc1c14529c1f5fd8
Dimitris Stavroulakis <jimstav@gmail.com>
2025-12-30 19:13:04 +0200

    Style Input component

--- src/components/NewProject.tsx
+++ src/components/NewProject.tsx
@@ -53,17 +53,22 @@ const NewProject = ({ onSaveProject, onCancel }: NewProjectProps) => {
   };
 
   return (
-    <div className="flex flex-col w-96 p-8 gap-6">
-      <menu className="flex justify-end gap-2">
-        <button onClick={handleCancel}>Cancel</button>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
         <button
-          className="rounded bg-black text-white py-1 px-4 hover:bg-neutral-700"
+          className="text-stone-800 hover:text-stone-950"
+          onClick={handleCancel}
+        >
+          Cancel
+        </button>
+        <button
+          className="px-6 py-2 rounded-md bg-stone-800 text-stone-50  hover:bg-stone-950"
           onClick={handleSave}
         >
           Save
         </button>
       </menu>
-      <div className="flex flex-col gap-4">
+      <div>
         <Input label="Title" />
         <Input label="Description" textarea />
         <Input label="Due Date" />
